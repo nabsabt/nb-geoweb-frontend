a9c9ad499ed1db8f35c4377efe4d375311985c81
nabsabt <nabsabt@gmail.com>
2025-07-18 13:12:53 +0200

    fragmant navigation/basic structure implemented

--- src/app/@Component/home/home.component.ts
+++ src/app/@Component/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
+  providers: [TranslateService],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(
+    private translateService: TranslateService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    translateService.setDefaultLang('en');
+    translateService.use('en');
+  }
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
+      if (fragment) this.jumpToSection(fragment);
+    });
+  }
+
+  public jumpToSection(section: string): void {
+    const element = document.getElementById(section);
+    if (element) {
+      const yOffset = window.innerWidth < 701 ? 75 : 100; // Adjust offset for mobile view
+      const y =
+        element.getBoundingClientRect().top + window.pageYOffset - yOffset;
+
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
+}
--- src/app/@Component/navbar/navbar.component.ts
+++ src/app/@Component/navbar/navbar.component.ts
@@ -1,21 +1,41 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
+  providers: [TranslateService],
 })
-export class NavbarComponent {
-  // Navbar logic can be added here in the future
+export class NavbarComponent implements OnInit {
   @ViewChild('navbarSupportedContent') navbarSupportedContent: ElementRef;
+
+  public changeToLangValue: string;
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+    translateService.use('en');
+  }
+  ngOnInit(): void {
+    this.changeToLangValue =
+      this.translateService.currentLang === 'en' ? 'hu' : 'en';
+  }
 
   public collapseNavbar() {
     const navbar = this.navbarSupportedContent.nativeElement;
     if (navbar.classList.contains('show')) {
       navbar.classList.remove('show');
     }
+  }
+
+  changeLanguage() {
+    this.translateService.currentLang === 'en'
+      ? this.translateService.use('hu')
+      : this.translateService.use('en');
+    this.changeToLangValue =
+      this.translateService.currentLang === 'en' ? 'hu' : 'en';
   }
 }

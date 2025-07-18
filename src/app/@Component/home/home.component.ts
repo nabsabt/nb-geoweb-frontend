24eeb47ca347cd20780e95e66bc123a0229ca0df
nabsabt <nabsabt@gmail.com>
2025-07-18 19:03:52 +0200

    beta version finished

--- src/app/@Component/home/home.component.ts
+++ src/app/@Component/home/home.component.ts
@@ -2,16 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { NgxVortexComponent } from '@omnedia/ngx-vortex';
+import { Dot, NgxMapComponent } from '@omnedia/ngx-map';
+import { NgxMarqueeComponent } from '@omnedia/ngx-marquee';
+import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [TranslateModule, NgxVortexComponent],
+  imports: [
+    TranslateModule,
+    NgxVortexComponent,
+    NgxMapComponent,
+    NgxMarqueeComponent,
+    NgxNeonUnderlineComponent,
+  ],
   providers: [TranslateService],
 })
 export class HomeComponent implements OnInit {
+  dots: Dot[] = [
+    {
+      start: { lat: 34.0522, lng: -118.2437 }, // Los Angeles, USA
+      end: { lat: 55.7558, lng: 37.6173 }, // Moscow, Russia
+    },
+    {
+      start: { lat: -33.8688, lng: 151.2093 }, // Sydney, Australia
+      end: { lat: 51.5074, lng: -0.1278 }, // London, UK
+    },
+    /* {
+      start: { lat: -33.9249, lng: 18.4241 }, // Cape Town, South Africa
+      end: { lat: 41.7151, lng: 44.8271 }, // Tbilisi, Georgia
+    }, */
+    {
+      start: { lat: 35.6895, lng: 139.6917 }, // Tokyo, Japan
+      end: { lat: 25.276987, lng: 55.296249 }, // Dubai, UAE
+    },
+  ];
+
   constructor(
     private translateService: TranslateService,
     private activatedRoute: ActivatedRoute

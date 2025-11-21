eb57dae68b3859c23bf8a1cfee938b6bd771b75c
nabsabt <nabsabt@gmail.com>
2025-11-21 20:55:54 +0100

    finished design almost

--- src/app/@Component/home/home.component.ts
+++ src/app/@Component/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [TranslateModule, RouterModule],
+  imports: [
+    TranslateModule,
+    RouterModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
 })
 export class HomeComponent implements OnInit {
@@ -28,6 +37,7 @@ export class HomeComponent implements OnInit {
 
   public jumpToSection(section: string): void {
     const element = document.getElementById(section);
+    console.log(element, section);
     if (element) {
       const yOffset = window.innerWidth < 701 ? 75 + 10 : 100 + 10; // Adjust offset for mobile view
       const y =
@@ -40,4 +50,6 @@ export class HomeComponent implements OnInit {
   public navigateToExternalPage(url: string) {
     window.open(url, '_blank');
   }
+
+  public onSubmitMessage(form: NgForm) {}
 }
